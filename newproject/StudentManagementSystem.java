7d6671af9632051cf7cfb020bcbf4dfe0f2449c1
renukapange12 <renukapange147@gmail.com>
2024-01-21 15:11:32 +0530

    Add modified files

---
+++ newproject/StudentManagementSystem.java
@@ -0,0 +1,39 @@
+/*
+ * StudentManagementSystem.java
+ * Coder: Namrata Ingle
+ * Copyright (c) [2023] Namrata Ingle. All rights reserved.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentManagementSystem {
+    private List<Student> students;
+
+    public StudentManagementSystem() {
+        this.students = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public void removeStudent(Student student) {
+        students.remove(student);
+    }
+
+    public Student searchStudent(int rollNumber) {
+        for (Student student : students) {
+            if (student.getRollNumber() == rollNumber) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    public void displayAllStudents() {
+        for (Student student : students) {
+            System.out.println(student);
+        }
+    }
+}
